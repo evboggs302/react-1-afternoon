db898b889dc57ef8409184eff0395231b06cbc92
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-05-06 15:03:56 -0700

    even and odd complete

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,7 +1,51 @@
 import React, { Component } from "react";
 
 export default class EvenAndOdd extends Component {
+  constructor() {
+    super();
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: ""
+    };
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+  sepEvenAndOdds() {
+    let bank = this.state.userInput.split("");
+    let eArr = [];
+    let oArr = [];
+    for (let i = 0; i < bank.length; i++) {
+      if (bank[i] % 2 !== 0) {
+        oArr.push(parseInt(bank[i]));
+      } else {
+        eArr.push(parseInt(bank[i]));
+      }
+    }
+  }
+
   render() {
-    return <p>EvenAndOdd Component</p>;
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens and Odds</h4>
+        <input
+          className="inputLine"
+          onChange={event => this.handleChange(event.target.value)}
+        />
+        <button
+          className="confirmationButton"
+          onClick={() => {
+            this.sepEvenAndOdds(this.state.userInput);
+          }}
+        >
+          Button
+        </button>
+        <span className="resultsBox">Evens: {this.state.evenArray}</span>
+        <span className="resultsBox">Odds: {this.state.oddArray}</span>
+      </div>
+    );
   }
 }
